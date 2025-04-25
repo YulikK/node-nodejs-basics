b036c6608adef64486cd494478f068159b4c08f9
YulikK <yulikpoch@gmail.com>
2025-04-25 19:20:09 +0200

    feat: implement file hash calculation using sha256

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+import { logMsg, getPathData } from '../utils.js';
+
+const { __dirname } = getPathData(import.meta.url);
+const dirName = 'files';
+const fileName = 'fileToCalculateHashFor.txt';
+const filePath = path.join(__dirname, dirName, fileName);
+const hashAlgorithm = 'sha256';
+
 const calculateHash = async () => {
-    // Write your code here 
+  logMsg('Starting work calcHash.js');
+
+  const hash = createHash(hashAlgorithm);
+
+  try {
+    const stream = createReadStream(filePath);
+
+    await pipeline(stream, hash);
+
+    logMsg(hash.digest('hex'), 'important');
+  } catch (err) {
+    logMsg(err, 'error');
+  }
+  logMsg('Ending work calcHash.js');
 };
 
 await calculateHash();
