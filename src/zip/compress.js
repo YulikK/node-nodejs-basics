50485fda22e457c83ba877c450f00810743d801e
YulikK <yulikpoch@gmail.com>
2025-04-26 13:55:42 +0200

    feat: implement file compression functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { logMsg, getPathData } from '../utils.js';
+
+const dirName = 'files';
+const sourceFileName = 'fileToCompress.txt';
+const archiveName = 'archive.gz';
+const { __dirname } = getPathData(import.meta.url);
+const sourcePath = path.join(__dirname, dirName, sourceFileName);
+const archivePath = path.join(__dirname, dirName, archiveName);
+
 const compress = async () => {
-    // Write your code here 
+  logMsg('Starting work compress.js');
+
+  const sourceStream = createReadStream(sourcePath);
+  const gzipStream = createGzip();
+  const destinationStream = createWriteStream(archivePath);
+
+  try {
+    await pipeline(sourceStream, gzipStream, destinationStream);
+  } catch (error) {
+    logMsg(error, 'error');
+  }
+
+  logMsg('Ending work compress.js');
 };
 
 await compress();
