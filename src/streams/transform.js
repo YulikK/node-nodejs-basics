5133e433262dd27d74c250abc62c2c240a491c0a
YulikK <yulikpoch@gmail.com>
2025-04-26 13:19:46 +0200

    feat: implement reverse transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+import { pipeline } from 'node:stream/promises';
+import { logMsg } from '../utils.js';
+
+const reverseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    const reversed =
+      chunk.toString().trim().split('').reverse().join('') + '\n';
+    callback(null, reversed);
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  logMsg('Starting work transform.js');
+
+  try {
+    await pipeline(stdin, reverseTransform, stdout, { end: false });
+  } catch (error) {
+    logMsg(error, 'error');
+  }
+
+  logMsg('Ending work transform.js');
 };
 
 await transform();
