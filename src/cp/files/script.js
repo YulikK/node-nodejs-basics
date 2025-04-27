92b4ab6e59f5e671cf52e125254f1705649905f0
YulikK <yulikpoch@gmail.com>
2025-04-27 11:32:03 +0200

    feat: implement child process spawning

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,12 +1,16 @@
+import { logMsg } from '../../utils.js';
 const args = process.argv.slice(2);
 
-console.log(`Total number of arguments is ${args.length}`);
-console.log(`Arguments: ${JSON.stringify(args)}`);
+logMsg(`Total number of arguments is ${args.length}`, 'important');
+logMsg(`Arguments: ${JSON.stringify(args)}`, 'important');
 
 const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  const chunkStringified = chunk.toString();
+  if (chunkStringified.includes('CLOSE')) {
+    logMsg('Ending work cp.js');
+    process.exit(0);
+  }
+  process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
 };
 
 process.stdin.on('data', echoInput);
