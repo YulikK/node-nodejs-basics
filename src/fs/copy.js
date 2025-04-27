8d0b73a4b691c4a6ad5962c3e97eb8f70ef38818
YulikK <yulikpoch@gmail.com>
2025-04-27 11:49:33 +0200

    refactor: improve error handling and code readability

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,12 +1,15 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import {logMsg, getPathData} from '../utils.js';
+import { logMsg, getPathData } from '../utils.js';
 
 const sourceDirName = 'files';
 const targetDirName = 'files_copy';
-const errorMessage = 'FS operation failed';
 const { __dirname } = getPathData(import.meta.url);
-
+const errorMap = {
+  ENOENT: 'FS operation failed',
+  EEXIST: 'FS operation failed',
+  ERR_FS_CP_EEXIST: 'FS operation failed',
+};
 const copy = async () => {
   logMsg('Starting work copy.js');
 
@@ -14,9 +17,16 @@ const copy = async () => {
   const targetPath = path.join(__dirname, targetDirName);
 
   try {
-    await fs.cp(sourcePath, targetPath, { recursive: true, errorOnExist: true, force: false });
-    logMsg(`Success: Directory '${sourceDirName}' copied to '${targetDirName}'`);
+    await fs.cp(sourcePath, targetPath, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+    logMsg(
+      `Success: Directory '${sourceDirName}' copied to '${targetDirName}'`
+    );
   } catch (error) {
+    const errorMessage = errorMap[error.code] || error.message;
     logMsg(`${errorMessage}. ${error}`, 'error');
   }
 };
